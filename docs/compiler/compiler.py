01d55171217932cf8aba06209ea1cbd7c72dbcf8
eodevx <daniel.claus@gymnasium-heiligenhaus.de>
2025-06-05 19:27:04 +0200

    Initial Commit

---
+++ docs/compiler/compiler.py
@@ -0,0 +1,120 @@
+import os
+import shutil
+
+from typing import List
+
+DEST = "../source/api"
+
+METHODS_TEMPLATE = open("template/methods.rst", encoding="UTF-8").read()
+TYPES_TEMPLATE = open("template/types.rst", encoding="UTF-8").read()
+ENUMS_TEMPLATE = open("template/enums.rst", encoding="UTF-8").read()
+
+def generate_documentation(
+    directory: str,
+    keys: List[str],
+    base_class: str,
+    template: str,
+    auto_type: str,
+    autodocs_args: List[str] = [],
+):
+    template_lines = []
+    root = DEST + directory
+    shutil.rmtree(root, ignore_errors=True)
+    os.mkdir(root)
+
+    for key in keys:
+        with open(root + f"/{key}.rst", "w") as f:
+            f.write(key + "\n" + "=" * len(key) + "\n\n")
+            f.write(f".. auto{auto_type}:: {base_class}.{key}()")
+            for arg in autodocs_args:
+                f.write(f"\n   :{arg}:")
+
+    # Build list
+    template_lines.extend([
+        "\n\n",
+        ".. autosummary::\n",
+        "   :nosignatures:\n",
+        "\n\n"
+    ])
+
+    for key in keys:
+        template_lines.append(f"    {key} <{key}>\n")
+
+    # Build TOC
+    template_lines.extend([
+        "\n\n",
+        ".. toctree::\n",
+        "   :hidden:\n",
+        "\n\n"
+    ])
+
+    for key in keys:
+        template_lines.append(f"    {key} <{key}>\n")
+
+    # Build template
+    for line in template_lines:
+        template += line
+    with open(root + "/index.rst", "w") as index:
+        index.write(template)
+        
+if "__main__" == __name__:
+    generate_documentation(
+        directory="/methods",
+        base_class="crunpyroll.Client",
+        auto_type="method",
+        template=METHODS_TEMPLATE,
+        keys=[
+            "search",
+            "delete_active_stream",
+            "get_series",
+            "get_seasons",
+            "get_episodes",
+            "get_profile",
+            "get_index",
+            "get_streams",
+            "get_license",
+            "get_manifest"
+        ]
+    )
+
+    generate_documentation(
+        directory="/types",
+        base_class="crunpyroll.types",
+        auto_type="class",
+        template=TYPES_TEMPLATE,
+        keys=[
+            "SearchQuery",
+            "Series",
+            "SeasonsQuery",
+            "Season",
+            "EpisodesQuery",
+            "Episode",
+            "Movie",
+            "Profile",
+            "SessionIndex",
+            "CMS",
+            "MediaStreams",
+            "SubtitlesStream",
+            "HardsubStream",
+            "Manifest",
+            "ManifestVideoStream",
+            "ManifestAudioStream",
+            "ContentProtection",
+            "DRM",
+            "Image",
+            "Images"
+        ]
+    )
+
+    generate_documentation(
+        directory="/enums",
+        base_class="crunpyroll.enums",
+        auto_type="class",
+        autodocs_args=["members"],
+        template=ENUMS_TEMPLATE,
+        keys=[
+            "ContentType",
+            "ImageType",
+            "APIHost"
+        ]
+    )
---
+++ docs/source/index.rst
@@ -0,0 +1,23 @@
+Welcome to Crunpyroll
+=====================
+
+Contents are organized into sections composed of self-contained topics which can be all accessed from the sidebar, or by
+following them in order using the :guilabel:`Next` button at the end of each page.
+You can also switch to Dark or Light theme or leave on Auto (follows system preferences) by using the dedicated button
+in the top left corner.
+
+.. toctree::
+    :hidden:
+    :caption: Introduction
+
+    intro/quickstart
+    intro/install
+
+.. toctree::
+    :hidden:
+    :caption: API Reference
+    
+    api/client
+    api/methods/index
+    api/types/index
+    api/enums/index
